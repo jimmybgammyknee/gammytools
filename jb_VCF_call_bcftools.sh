65009d334d0981617ed5bc7e94100477489f6b22
jimmybgammyknee <jimmymbreen@gmail.com>
2015-02-05 09:15:59 +1030

    added script that does variant calling using acad server executibles and new version of samtools and bcftools

---
+++ jb_VCF_call_bcftools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# Jimmy (jimmymbreen@gmail.com)	-	20150205
+# Call Variants using BCFtools from BAM file
+
+if [ "$#" != "3" ]; then
+        echo "Usage: jb_VCF_call_bcftools.sh [Reference] [BAM]"
+        exit 0
+fi 
+
+ref=$1
+bam=$2
+
+bcf=/opt/local/samtools_1_1/bcftools-1.1/bcftools
+sam=/opt/local/samtools_1_1/samtools-1.1/samtools
+
+$sam mpileup -ugf $ref $bam | $bcf call -vmO z -o $bam.vcf.gz
