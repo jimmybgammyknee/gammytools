aa4a3140626e15ae9d6dafae7e5219b834bada15
jimmybgammyknee <jimmymbreen>
2015-03-28 15:13:52 +1030

    converts gaff from blast std

---
+++ jb_blast_std2gff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+# Jimmy (jimmymbreen@gmail.com) -       20150328
+# Produce gff file from a xml -> std tabular format
+
+if [ "$#" != "1" ]; then
+        echo "Usage: jb_blast_std2gff.sh [BLAST std tab]"
+        exit 0
+fi
+
+stdin=$1
+
+awk '{print $1"\t"".""\t""blast""\t"$7"\t"$8"\t"".""\t""+""\t"".""\t""Parent="$2}' > ${stdin%%.*}.gff
