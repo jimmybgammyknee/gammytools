f1c311650a351ea05797480ae77e201a8f914192
jimmybgammyknee <jimmymbreen>
2016-12-21 12:45:36 +1030

    TSS plot extend to 5kb

--- jb_TSSplot_Vv.sh
+++ jb_TSSplot_Vv.sh
@@ -10,19 +10,19 @@ tss=/localscratch/Refs/PlantGenomes/Vitis_vinifera/Vvinifera_145_Genoscope.12X.g
 sizes=/localscratch/Refs/PlantGenomes/Vitis_vinifera/Vvinifera_145_Genoscope_12X_Chromosomes.sizes
 
 # extend tss
-bedtools slop -b 1000 -i $tss -g $sizes > TSS_updown_window.bed
+bedtools slop -b 5000 -i $tss -g $sizes > TSS_updown_5kb_slop.bed
 
 # Prepare the tiled windows from TSS file
-bedtools makewindows -b TSS_updown_window.bed -w 50 -i winnum | sort -k1,1 -k2,2n > TSS_50bp_updown_window.bed
+bedtools makewindows -b TSS_updown_5kb_slop.bed -w 50 -i winnum | sort -k1,1 -k2,2n > TSS_50bp_updown_5kb_slop.bed
 
 # Make sure bed file is sorted
 sort-bed <(zcat $file) > sorted.bed
 
 # Get average methylation % over windows
 # Get the average over ALL windows of all genes
-bedtools map -a TSS_50bp_updown_window.bed -b sorted.bed -c 5 -o mean -null 0 \
+bedtools map -a TSS_50bp_updown_5kb_slop.bed -b sorted.bed -c 5 -o mean -null 0 \
 	| sort -t$'\t' -k4,4n \
 	| bedtools groupby -i - -g 4 -c 5 -o mean > ${file}.TSS.counts.txt 
 
 # Clean up
-rm sorted.bed TSS_50bp_updown_window.bed TSS_updown_window.bed
+rm sorted.bed TSS_50bp_updown_5kb_slop.bed TSS_updown_5kb_slop.bed
