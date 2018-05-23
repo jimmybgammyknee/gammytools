208474b11a2973843778400331fecaad06e0eeaf
jimmybgammyknee <jimmymbreen@gmail.com>
2018-05-23 15:56:19 +0800

    remove comments that were causing issues

--- ATACseq_call_ATAC_peaks.sh
+++ ATACseq_call_ATAC_peaks.sh
@@ -20,11 +20,8 @@ mkdir -p ${outdir}
 
 ## Now we can run MACS2
 for i in ${data}/*.sorted.bam; do
-    macs2 callpeak --broad \            # MACS2 callpeaks function aggregating nearby peaks
-        --format BAM \                      # let MACS2 use paired end information
-        --nomodel --shift -37 --extsize 73 \  # Don't use default shift, instead use -37 and 73
-        --bdg \                               # Create a bedgraph output
-        --gsize 2.5e9 \                       # The mappable genome size
-        -t $i \                               # Treatment file
-        -n ${outdir}/$(basename ${i} .sorted.bam)        # Output prefix
+    macs2 callpeak --broad --format BAM \
+        --nomodel --shift -37 --extsize 73 \
+        --bdg --gsize 2.5e9 -t $i \
+        -n ${outdir}/$(basename ${i} .sorted.bam)
 done
